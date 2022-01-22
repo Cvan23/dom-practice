d6f3979f092627b4797953025b06d5410e120db9
Chris Van Huffe <cvanhuffel@outlook.com>
2022-01-22 14:11:09 -0800

    Event Listeners updated

--- js-exercises/event-listeners/script.js
+++ js-exercises/event-listeners/script.js
@@ -6,11 +6,26 @@ const child2 = document.querySelector(".child2");
 const audio1 = new Audio("zapsplat1.mp3");
 
 grandparent.addEventListener('click', (e) => {
-  console.log('hello buttface!',(e));
-  alert('Hello Buttface!');
+  console.log('HELLO BUTTFACE!!!',(e));
+  alert('HELLO BUTTFACE!!!');
 });
 
 parent.addEventListener('click', (e) => {
   audio1.play();
+
+  e.stopPropagation();
+});
+
+child1.addEventListener('click', (e) => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+
+  e.stopPropagation();
+});
+
+child2.addEventListener('click', (e) => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  grandparent.style.backgroundColor = "#" + randomColor;
+
   e.stopPropagation();
 });
